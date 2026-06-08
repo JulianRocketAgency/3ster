0803be1315d148cf35d493d3626b6a0e46e3bf98
Julian van Beek <julian@steakstore.nl>
2026-06-08 17:45:36 +0200

    Update vite config for production

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
     proxy: {
-      '/api': 'http://localhost:3001'
+      '/api': process.env.VITE_API_URL || 'http://localhost:3001'
     }
   }
 })
